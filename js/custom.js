cbe5789238156a2cf9720ce69e6d9bd11df1c0c7
Yuvadhara-Panangangara <shafeershan7@gmail.com>
2026-03-07 01:48:47 +0530

    Updating 07-03-26
    Blog page responsiveness completed, Digital library added as tab to mini library

--- js/custom.js
+++ js/custom.js
@@ -1,398 +1,129 @@
 /*---------------------------------------------------------------------
     File Name: custom.js
+    Description: Combined site logic and Library Tab Search
 ---------------------------------------------------------------------*/
 
 $(function () {
-	
-	"use strict";
-	
-	/* Preloader
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	setTimeout(function () {
-		$('.loader_bg').fadeToggle();
-	}, 1500);
-	
-	/* JQuery Menu
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-
-	$(document).ready(function () {
-		$('header nav').meanmenu();
-	});
-	
-	/* Tooltip
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$('[data-toggle="tooltip"]').tooltip();
-	});
-	
-	/* sticky
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".sticky-wrapper-header").sticky({topSpacing:0});
-	});
-	
-	/* Mouseover
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function(){
-		$(".main-menu ul li.megamenu").mouseover(function(){
-			if (!$(this).parent().hasClass("#wrapper")){
-			$("#wrapper").addClass('overlay');
-			}
-		});
-		$(".main-menu ul li.megamenu").mouseleave(function(){
-			$("#wrapper").removeClass('overlay');
-		});
-	});
-	
-	/* NiceScroll
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if ($.fn.niceScroll) {
-        $(".brand-box").niceScroll({
-            cursorcolor:"#9b9b9c",
-        });	
-    }
-	
-	/* NiceSelect
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-		$('select').niceSelect();
-	});	
-		
-	/* OwlCarousel - Blog Post slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-	  var owl = $('.carousel-slider-post');
-	  owl.owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 10,
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true
-	  });	  
-	});
-	
-	/* OwlCarousel - Banner Rotator Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-	  var owl = $('.banner-rotator-slider');
-	  owl.owlCarousel({
-		items: 1,
-		loop: true,
-		margin: 10,
-		nav: true,
-		dots: false,
-		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		autoplay: true,
-		autoplayTimeout: 3000,
-		autoplayHoverPause: true
-	  });	  
-	});
-	
-	/* OwlCarousel - Product Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	
-	$(document).ready(function() {
-	  var owl = $('#product-in-slider');
-	  owl.owlCarousel({
-		loop: true,
-		nav: true,
-		margin: 10,
-		navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
-		responsive: {
-		  0: {
-			items: 1
-		  },
-		  600: {
-			items: 2
-		  },
-		  960: {
-			items: 3
-		  },
-		  1200: {
-			items: 4
-		  }
-		}
-	  });
-	  owl.on('mousewheel', '.owl-stage', function(e) {
-		if (e.deltaY > 0) {
-		  owl.trigger('next.owl');
-		} else {
-		  owl.trigger('prev.owl');
-		}
-		e.preventDefault();
-	  });
-	});
-	
-	/* Scroll to Top
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	$(window).on('scroll', function (){
-        var scroll = $(window).scrollTop();
-        if (scroll >= 100){
-          $("#back-to-top").addClass('b-show_scrollBut')
-        }else{
-          $("#back-to-top").removeClass('b-show_scrollBut')
-        }
-      });
-
-      $("#back-to-top").on("click", function(){
-        $('body,html').animate({
-          scrollTop: 0
-        }, 1000);
-    });
-	
-	/* Contact-form
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if ($.validator) {
-        $.validator.setDefaults( {
-            submitHandler: function () {
-                alert( "submitted!" );
-            }
-        });
-        
-        $( document ).ready( function () {
-            $( "#contact-form" ).validate( {
-                rules: {
-                    firstname: "required",
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    lastname: "required",
-                    message: "required",
-                    agree: "required"
-                },
-                messages: {
-                    firstname: "Please enter your firstname",
-                    email: "Please enter a valid email address",
-                    lastname: "Please enter your lastname",
-                    message: "Please enter your Message",
-                    agree: "Please accept our policy"
-                },
-                errorElement: "div",
-                errorPlacement: function ( error, element ) {
-                    error.addClass( "help-block" );
-                    if ( element.prop( "type" ) === "checkbox" ) {
-                        error.insertAfter( element.parent( "input" ) );
-                    } else {
-                        error.insertAfter( element );
-                    }
-                },
-                highlight: function ( element, errorClass, validClass ) {
-                    $( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-error" ).removeClass( "has-success" );
-                },
-                unhighlight: function (element, errorClass, validClass) {
-                    $( element ).parents( ".col-md-4, .col-md-12" ).addClass( "has-success" ).removeClass( "has-error" );
-                }
-            });
-        });
-    }
-	
-	/* heroslider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if (typeof Swiper !== 'undefined') {
-        var swiperHero = new Swiper('.heroslider', {
-            spaceBetween: 30,
-            centeredSlides: true,
-            slidesPerView: 'auto',
-            paginationClickable: true,
-            loop: true,
-            autoplay: {
-                delay: 2500,
-                disableOnInteraction: false,
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                dynamicBullets: true
-            },
-        });
-
-        /* Product Filters */
-        var swiperFilters = new Swiper('.swiper-product-filters', {
-            slidesPerView: 3,
-            slidesPerColumn: 2,
-            spaceBetween: 30,
-            breakpoints: {
-                1024: { slidesPerView: 3, spaceBetween: 30 },
-                768: { slidesPerView: 2, spaceBetween: 30, slidesPerColumn: 1 },
-                640: { slidesPerView: 2, spaceBetween: 20, slidesPerColumn: 1 },
-                480: { slidesPerView: 1, spaceBetween: 10, slidesPerColumn: 1 }
-            },
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-                dynamicBullets: true
-            }
-        });
-    }
-
-	/* Countdown
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if ($.fn.countdown) {
-        $('[data-countdown]').each(function () {
-            var $this = $(this),
-            finalDate = $(this).data('countdown');
-            $this.countdown(finalDate, function (event) {
-                $(this).html(event.strftime(''
-                + '<div class="time-bar"><span class="time-box">%w</span> <span class="line-b">weeks</span></div> '
-                + '<div class="time-bar"><span class="time-box">%d</span> <span class="line-b">days</span></div> '
-                + '<div class="time-bar"><span class="time-box">%H</span> <span class="line-b">hr</span></div> '
-                + '<div class="time-bar"><span class="time-box">%M</span> <span class="line-b">min</span></div> '
-                + '<div class="time-bar"><span class="time-box">%S</span> <span class="line-b">sec</span></div>'));
-            });
-        });
-    }
-	
-	/* Deal Slider
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if ($.fn.slick) {
-        $('.deal-slider').slick({
-            dots: false,
-            infinite: false,
-            prevArrow: '.previous-deal',
-            nextArrow: '.next-deal',
-            speed: 500,
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            responsive: [
-                { breakpoint: 1024, settings: { slidesToShow: 3, slidesToScroll: 2, infinite: true } },
-                { breakpoint: 768, settings: { slidesToShow: 2, slidesToScroll: 2 } },
-                { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-            ]
-        });
-
-        /* News Slider */
-        $('#news-slider').slick({
-            dots: false,
-            infinite: false,
-            prevArrow: '.previous',
-            nextArrow: '.next',
-            speed: 500,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            responsive: [
-                { breakpoint: 1024, settings: { slidesToShow: 1, slidesToScroll: 1, infinite: true } },
-                { breakpoint: 600, settings: { slidesToShow: 1, slidesToScroll: 1 } },
-                { breakpoint: 480, settings: { slidesToShow: 1, slidesToScroll: 1 } }
-            ]
-        });
-    }
-	
-	/* Fancybox
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-	if ($.fn.fancybox) {
-        $(".fancybox").fancybox({
-            maxWidth: 1200,
-            maxHeight: 600,
-            width: '70%',
-            height: '70%',
-        });
-    }
-	
-	/* Toggle sidebar
-	-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     $(document).ready(function () {
-       $('#sidebarCollapse').on('click', function () {
-          $('#sidebar').toggleClass('active');
-          $(this).toggleClass('active');
-       });
-     });
-
-     /* Product slider 
-     -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-     $('#blogCarousel').carousel({
-        interval: 5000
-     });
-
-    /* Library Search Functionality 
-    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
-    const searchInput = document.getElementById('search-input');
-    const books = document.querySelectorAll('.book');
-
-    if (searchInput) {
-        searchInput.addEventListener('input', (event) => {
-            const searchTerm = event.target.value.toLowerCase();
-            
-            books.forEach(book => {
-                const title = book.querySelector('h4').textContent.toLowerCase();
-                const author = book.querySelector('p').textContent.toLowerCase();
-                
-                // Show/hide based on search term. Empty string style allows Flexbox to work correctly.
-                if (title.includes(searchTerm) || author.includes(searchTerm)) {
-                    book.style.display = ''; 
-                } else {
-                    book.style.display = 'none';
-                }
-            });
-        });
-    }
-
-}); // End of main function
-
-/* dropdown menu start */
-$(document).ready(function() {
-    $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
-        if (!$(this).next().hasClass('show')) {
-            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
-        }
-        var $subMenu = $(this).next(".dropdown-menu");
-        $subMenu.toggleClass('show');
-
-        $(this).parents('li.nav-item.dropdown.show').on('hidden.bs.dropdown', function(e) {
-            $('.dropdown-submenu .show').removeClass("show");
-        });
-
-        return false;
-    });
-});
-/* dropdown menu end */
-
-/* Blog Read More & Share Button Logic */
-document.addEventListener('DOMContentLoaded', function() {
-  const blogCards = document.querySelectorAll('.blog-card');
-
-  blogCards.forEach(card => {
-    const readMoreBtn = card.querySelector('.read-more-btn');
-    const expandableText = card.querySelector('.expandable-text');
-    const shareBtn = card.querySelector('.share-btn');
     
-    // Safety check for blog title element
-    const titleEl = card.querySelector('h1');
-    const blogTitle = titleEl ? titleEl.textContent : "Blog Post";
-
-    if (readMoreBtn && expandableText) {
-      readMoreBtn.addEventListener('click', function() {
-        card.classList.toggle('expanded');
-        if (card.classList.contains('expanded')) {
-          readMoreBtn.textContent = 'Read less';
-          expandableText.style.maxHeight = expandableText.scrollHeight + 'px';
-        } else {
-          readMoreBtn.textContent = 'Read more';
-          expandableText.style.maxHeight = '100px';
+    "use strict";
+    
+    /* 1. Preloader
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    setTimeout(function () {
+        $('.loader_bg').fadeToggle();
+    }, 1500);
+    
+    /* 2. Menu & Sticky
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    $(document).ready(function () {
+        if ($.fn.meanmenu) {
+            $('header nav').meanmenu();
         }
-      });
-    }
-
-    if (shareBtn) {
-      shareBtn.addEventListener('click', () => {
-        if (navigator.share) {
-            navigator.share({
-              title: blogTitle,
-              text: 'Check out this blog post: ' + blogTitle,
-              url: window.location.href
-            }).catch(console.error);
-        } else {
-            alert('Sharing is not supported on your browser. Please copy and paste the URL to share.');
+        if ($.fn.sticky) {
+            $(".sticky-wrapper-header").sticky({topSpacing:0});
         }
-      });
-    }
-  });
-});
+        $('[data-toggle="tooltip"]').tooltip();
+    });
+
+    /* 3. Owl Carousels (Post, Banner, Product)
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    $(document).ready(function() {
+        if ($.fn.owlCarousel) {
+            // Post Slider
+            $('.carousel-slider-post').owlCarousel({
+                items: 1, loop: true, margin: 10, autoplay: true, autoplayTimeout: 3000
+            });
+            
+            // Banner Rotator
+            $('.banner-rotator-slider').owlCarousel({
+                items: 1, loop: true, margin: 10, nav: true, dots: false,
+                navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+                autoplay: true, autoplayTimeout: 3000
+            });
+
+            // Product Slider
+            var productSlider = $('#product-in-slider');
+            productSlider.owlCarousel({
+                loop: true, nav: true, margin: 10,
+                navText : ["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+                responsive: { 0: {items: 1}, 600: {items: 2}, 960: {items: 3}, 1200: {items: 4} }
+            });
+        }
+    });
+
+    /* 4. Scroll to Top
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    $(window).on('scroll', function (){
+        if ($(window).scrollTop() >= 100){
+            $("#back-to-top").addClass('b-show_scrollBut');
+        } else {
+            $("#back-to-top").removeClass('b-show_scrollBut');
+        }
+    });
+
+    $("#back-to-top").on("click", function(){
+        $('body,html').animate({ scrollTop: 0 }, 1000);
+    });
+
+    /* 5. Library Search Logic (PAPERBACK & E-BOOKS)
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    $(document).ready(function() {
+        // This listens to any search input with the class 'lib-search'
+        $('.lib-search').on('input', function() {
+            const searchTerm = $(this).val().toLowerCase();
+            // It finds the target list (paper-list or ebook-list) from the HTML attribute
+            const targetListId = $(this).data('target');
+            const books = $('#' + targetListId + ' .book');
+
+            books.each(function() {
+                const title = $(this).find('h4').text().toLowerCase();
+                const author = $(this).find('p').text().toLowerCase();
+                
+                // Matches title or author
+                if (title.includes(searchTerm) || author.includes(searchTerm)) {
+                    $(this).show(); 
+                } else {
+                    $(this).hide();
+                }
+            });
+        });
+    });
+
+    /* 6. Blog Read More & Share Logic
+    -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- */
+    $(document).ready(function() {
+        const blogCards = document.querySelectorAll('.blog-card');
+        blogCards.forEach(card => {
+            const readMoreBtn = card.querySelector('.read-more-btn');
+            const expandableText = card.querySelector('.expandable-text');
+            const shareBtn = card.querySelector('.share-btn');
+
+            if (readMoreBtn && expandableText) {
+                readMoreBtn.addEventListener('click', function() {
+                    card.classList.toggle('expanded');
+                    if (card.classList.contains('expanded')) {
+                        readMoreBtn.textContent = 'Read less';
+                        expandableText.style.maxHeight = expandableText.scrollHeight + 'px';
+                    } else {
+                        readMoreBtn.textContent = 'Read more';
+                        expandableText.style.maxHeight = '100px';
+                    }
+                });
+            }
+
+            if (shareBtn) {
+                shareBtn.addEventListener('click', () => {
+                    if (navigator.share) {
+                        navigator.share({
+                            title: 'Blog Post',
+                            url: window.location.href
+                        }).catch(console.error);
+                    } else {
+                        alert('Sharing not supported on this browser.');
+                    }
+                });
+            }
+        });
+    });
+
+}); // End of File
